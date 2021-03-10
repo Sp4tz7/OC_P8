4800dae38875a7611d15a61c25d12b1b5a724bb3
David <ds@magneticlab.ch>
2021-03-10 10:26:59 +0100

    login tests assertions

--- tests/Controller/DefaultControllerTest.php
+++ tests/Controller/DefaultControllerTest.php
@@ -2,17 +2,39 @@
 
 namespace App\Tests\Controller;
 
+use App\DataFixtures\UserFixture;
+use App\Repository\UserRepository;
+use Liip\TestFixturesBundle\Test\FixturesTrait;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Symfony\Component\HttpFoundation\Response;
 
 class DefaultControllerTest extends WebTestCase
 {
-    public function testIndex()
+    use FixturesTrait;
+
+    private $client;
+
+    public function setUp():void
     {
-        $client = static::createClient();
-
-        $client->request('GET', '/');
-
-        $this->assertEquals(302, $client->getResponse()->getStatusCode());
-        //$this->assertResponseRedirects('/login');
+        $this->client = static::createClient();
+        $this->loadFixtures([UserFixture::class]);
+        $userRepository = self::$container->get(UserRepository::class);
+        $this->user     = $userRepository->findOneBy(['email' => 'userone@todoandco.com']);
+        $this->admin    = $userRepository->findOneBy(['email' => 'admin@todoandco.com']);
     }
+
+    public function testRedirectToLogin()
+    {
+        $this->client->request('GET', '/');
+        $this->assertResponseStatusCodeSame(Response::HTTP_FOUND);
+
+    }
+
+    public function testAuthenticatedUserAccessHome()
+    {
+        $this->client->loginUser($this->user);
+        $this->client->request('GET', '/');
+        $this->assertResponseStatusCodeSame(Response::HTTP_OK);
+    }
+
 }
